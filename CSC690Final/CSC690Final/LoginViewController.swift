4ab2be6ee4d619ca60137438e7e5d08820141437
Kurtis Hoang <KurtisHoang@Kurtiss-MBP.attlocal.net>
2018-12-09 17:39:57 -0800

    added the base project

---
+++ CSC690Final/CSC690Final/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CSC690Final
+//
+//  Created by Kurtis Hoang on 12/9/18.
+//  Copyright © 2018 Kurtis Hoang. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
