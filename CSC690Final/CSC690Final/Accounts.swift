504104b92cee851345a11621c49bc03c45f906e4
Kurtis Hoang <KurtisHoang@Kurtiss-MBP.attlocal.net>
2018-12-09 18:50:21 -0800

    added segue

---
+++ CSC690Final/CSC690Final/Accounts.swift
@@ -0,0 +1,20 @@
+//
+//  Accounts.swift
+//  CSC690Final
+//
+//  Created by Kurtis Hoang on 12/9/18.
+//  Copyright © 2018 Kurtis Hoang. All rights reserved.
+//
+
+import UIKit
+
+class Accounts {
+    var username: String
+    var password: String
+    
+    init(newUsername: String, newPassword: String)
+    {
+        username = newUsername
+        password = newPassword
+    }
+}
--- CSC690Final/CSC690Final/LoginViewController.swift
+++ CSC690Final/CSC690Final/LoginViewController.swift
@@ -5,11 +5,10 @@
 //  Created by Kurtis Hoang on 12/9/18.
 //  Copyright © 2018 Kurtis Hoang. All rights reserved.
 //
-
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    
     //stores the text from the username textfield
     @IBOutlet weak var usernameField: UITextField!
     //stores the text from the password textfield
@@ -19,15 +18,14 @@ class LoginViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     //when log in button is pressed
     @IBAction func LoginButtonPressed(_ sender: Any) {
-    
+        
     }
     
     @IBAction func SignupButtonPressed(_ sender: Any) {
-    
+        
     }
     
 }
-
