0ec65cf9c3ba2a711435d0df4f77af27776f7c6b
Kurtis Hoang <KurtisHoang@Kurtiss-MBP.attlocal.net>
2018-12-09 21:38:50 -0800

    added more stuff to storyboard and added two files

---
+++ CSC690Final/CSC690Final/SignupViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SignupViewController.swift
+//  CSC690Final
+//
+//  Created by Kurtis Hoang on 12/9/18.
+//  Copyright © 2018 Kurtis Hoang. All rights reserved.
+//
+
+import UIKit
+
+class SignupViewController: UIViewController {
+    
+    //variables
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    //when create account button is presseds
+    @IBAction func CreateAccountButtonPressed(_ sender: Any) {
+    
+    }
+    
+    
+}
---
+++ CSC690Final/CSC690Final/profileViewController.swift
@@ -0,0 +1,33 @@
+//
+//  profileViewController.swift
+//  CSC690Final
+//
+//  Created by Kurtis Hoang on 12/9/18.
+//  Copyright © 2018 Kurtis Hoang. All rights reserved.
+//
+
+import UIKit
+
+class profileViewController: UIViewController {
+    
+    //variables
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    //when edit button is pressed
+    @IBAction func editButtonPressed(_ sender: Any) {
+    
+    }
+    
+    //when view pets button is pressed
+    @IBAction func viewpetsButtonPressed(_ sender: Any) {
+    }
+}
